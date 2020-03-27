98b63b44ab4df330f1882a0eedfb474bfa068826
Chase Smith <chasesmith@chases-mbp.google.com>
2020-03-27 14:07:59 -0700

    adding chainlink contract integration

--- Derivative.sol
+++ Derivative.sol
@@ -5,28 +5,20 @@ pragma solidity >=0.4.22 <0.7.0;
  * @dev Store & retreive value in a variable
  */
 contract Derivative {
+    string name;
+    string symbol;
 
-    string assetName;
-    string derivateSymbol;
-    string derivativeName;
-    string baseAssetName;
-    address oracle;
-    address assetContractAddress;
-    address baseAssetContractAddress;
-    uint256 buffer;
-
-
+    address[] users;
     mapping(address => uint256) shorts;
     mapping(address => uint256) longs;
-    address[] users;
-    mapping(address => uint256) balances;
-
-
     uint256 totalShort;
     uint256 totalLong;
 
-    uint256 assetPrice;
+    mapping(address => uint256) balances;
+    address baseAsset; //0xdac17f958d2ee523a2206206994597c13d831ec7
 
+    uint256 assetPrice;
+    address oracleAddress;
 
 
     uint256 offset; //something mod the entire duration
@@ -37,43 +29,21 @@ contract Derivative {
 
 
     uint256 nextTimeStamp;
-    uint256 margin;
 
         //modifiers
 
-        //check collateral and exposure
-            modifier  {
-        require(
-            msg.sender == owner,
-            "Only owner can call this function."
-        );
-        _;
-    }
-
-
-        modifier notPaused {
-                  require(
-                0 == 0,
-             "."
-            );
+        //onlyOwner
+        modifier onlyOwner { require(msg.sender == this.owner, "Function can only be run by the owner of this contract.");
             _;
         }
-        //contract is active/not paused or in stages
-            modifier onlySettling {
-        require(
-            0 == 0,
-            "."
-        );
-        _;
-    }
 
-        modifier onlyActive {
-        require(
-            0 == 0,
-            "."
-        );
+        //
+        //contract is active/not paused or in stages
+        modifier onlySettling { require(0 == 0, "." );
         _;
-    }
+      }
+
+
 
     modifier onlyIssuing {
     require(
@@ -83,60 +53,53 @@ contract Derivative {
         _;
     }
 
-    modifier isSettled {
-        require(
-                0 == 0,
-                "."
-            );
-            _;
-    }
-
-
-
+    modifier isSettled { require(0 == 0, "."); _;  }
 
       /**
      * @dev Create a new derivative for an asset and range
      * @param {probability, settlementTime,
      */
-    constructor(bytes[] memory derivativeAssetData) public {}
+constructor(bytes[] memory derivativeAssetData) public {
+      super()
+      this.vaultFee = 100
+      this.oracle = "" //does this need to be created?
+      this.name = ""
+      this.symbol = ""
+  }
+
+function transitionDerivativeState() {
+           blockstamp = 4
+           if (this.State == Active && blockstamp <= this.activeCutoff) {
+               this.State = Settling
+               this.isSettled = this.settleAllPositions()
+               this.lastTimeStamp = blockstamp
+           }
+
+           if (this.State == Settling && this.isSettled && it is the correct timestamp) {
+                   this.State = Issuing
+           }
+
+           if (this.State == Issuing && it is the correct timestamp) {
+                   this.State = Active
+           }
+}
 
 
-    function issueDerivative(uint256 amount) public onlyIssuing verifiedAccount {
-        //calculate numbers on both ends
-        //amount to shares long and short
-        userLongs = 0
-        userShorts = 0
+function receive(address contract, address from, uint256 amount) private external returns (bool) {
+  if (contract.transferFrom(from, this, amount)) {
+    transferFrom(contract, this, amount)
+    return true
+  }
+  return false
+}
 
-        //mint the tokens
-        //base value for an account, with quantity based on some other thing
-        //amount is taken by the thing ..
-        //get x shorts, and y longs
-    }
+function issueDerivative(address purchaser, uint256 amount) public onlyIssuing {
+        amountAsset = amount/this.getPrice()
+        this.shorts[user] += amountAsset
+        this.userLongs[user] += amountAsset
+}
 
-
-    function withdraw(uint256 amount) public {
-        //transfer to the address that desires it
-        //return (this.getExposure() <= this.getAccoountBalance(msg.sender) )
-    }
-
-    function transitionDerivativeState() {
-        blockstamp = 4
-        if (this.State == Active && blockstamp <= this.activeCutoff) {
-            this.State = Settling
-            this.isSettled = this.settleAllPositions()
-            this.lastTimeStamp = blockstamp
-
-        }
-        if (this.State == Settling && this.isSettled && it is the correct timestamp) {
-                this.State = Issuing
-        }
-
-        if (this.State == Issuing && it is the correct timestamp) {
-                this.State = Active
-        }
-    }
-
-function settleAllPositions() public onlyActive onlyEnd (bool) {
+function settleAllPositions() public onlyActive onlyOnEnd (bool) {
     while (this.users.length > 0) {
         user = this.users.pop()
         this.settle(user)
@@ -144,49 +107,95 @@ function settleAllPositions() public onlyActive onlyEnd (bool) {
     return true
 }
 
-    function settle(address) private {
+function settle(address user) private {
         userLongs = this.longs[user]
         userShorts = this.shorts[user]
-        change = (userLongs - userShorts)*this.assetPrice
-        if (change) < 0 {
-            change = change * -1
-        }
+        change = (userLongs - userShorts)*this.getPrice()
         this.longs[user] = 0
         this.shorts[user] = 0
         this.totalLong -= userLongs
         this.totalShort -= userShorts
         this.balances[user] += change
+}
+
+   function isSafeTransfer(address sender, address receiver, uint256 shorts, uint256 longs) public (bool) {
+     if (this.longs[sender] >= longs && this.shorts[sender] >= shorts) {
+     senderLongs = this.longs[sender] - longs
+     senderShorts = this.shorts[sender] - shorts
+     senderExposure = this.calculateExposure(senderLongs, senderShorts, this.getPrice())
+
+     receiverLongs = this.longs[receiver] + longs
+     receiverShorts = this.shorts[receiver] + shorts
+     receiverExposure = this.calculateExposure(receiverLongs, receiverShorts, this.getPrice())
+     return (receiverExposure < this.balances[receiver]) && (senderExposure < this.balances[receiver]) }
+     else {
+       return false
+     }
+   }
+
+   function transferFrom(address from, address to, uint256 shorts, uint256 longs) public (bool) {
+      if (this.isSafeTransfer(from, to, shorts, longs)) {
+        this.longs[sender] -= longs
+        this.longs[receiver] += longs
+        this.shorts[sender]-= shorts
+        this.shorts[receiver] += shorts
+        return true
+      } else {
+        return false
+      }
+   }
+
+    function getAccountBalance(user) public (uint256) {
+        return this.balances[user]
     }
 
-    function getAccoountBalance(address user) public (uint256) {
-        return this.balances[user]
+    function accountWithdrawal(uint256 amount) public external returns (bool) {
+      if (amount <= this.balances[msg.sender]) {
+          this.balances[msg.sender] -= amount
+          this.baseAsset.transferFrom(this, msg.sender, amount)
+      }
+    }
+
+    function accountDeposit(uint256 amount) public returns (bool) {
+      receive(this.baseAsset, this, msg.sender, amount)
+      this.accounts[] += amount
     }
 
     function setAccountBalance(address user, uint256 newBalance) {
         this.balances[user] = newBalance
     }
 
-    function setPrice(uint256 newPrice) {
-        this.assetPrice = newPrice
+    function updatePrice() public external  {
+      oracleTimeStamp = this.oracle.getLatestTimestamp()
+      updatedPrice = this.getPrice()
+      if (oracleTimeStamp === this.timeStamp) {
+        updatedPrice = this.oracle.getLatestAnswer()
+      }
+      if (oracleTimeStamp > this.timestamp) {
+        back = oracleTimeStamp - this.timestamp
+        updatedPrice = this.oracle.getPreviousAnswer(back)
+      }
+      this.setPrice(updatedPrice)
     }
 
-    function getPrice(uint256 time) public payable {
-        newPrice =1000    //get from oracle
-        this.setPrice(newPrice)
-        return this.prce
+    function setPrice(uint256 newPrice) {
+        this.price = newPrice
     }
+    //hmmmm
+    function getPrice() public returns (uint256) {
+        return this.price
+    }
+    
 
     function updateExposure(address user) public {
         this.exposure[user] = calculateExposure(this.longs[user], this.shorts[user], this.getPrice())
     }
 
-    function calculateExposure(uint256 longs, uint256 shorts, uint256 price) public (uint256) {
+    function calculateExposure(uint256 longs, uint256 shorts, uint256 price) public returns (uint256) {
         return price * abs(shorts - longs)
     }
 
-    function getExposure(address user) public (uint256) {
-        return this.exposure[user]
+    function getExposure(address user) public returns (uint256) {
+        return calculateExposure(this.longs[user], this.shorts[user], this.getPrice())///
     }
-
-
 }
